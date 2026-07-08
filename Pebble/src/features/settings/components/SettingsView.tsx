4a07cb6c0e08286b53d0ea7db99d9ba7eef3d55d
Chae102 <coth4498@naver.com>
2026-07-08 15:50:50 +0900

    feat: 설정 섹션 헤더 컴포넌트 추가

--- Pebble/src/features/settings/components/SettingsView.tsx
+++ Pebble/src/features/settings/components/SettingsView.tsx
@@ -1,13 +1,30 @@
+import BellIcon from '@/assets/icons/bell-outline.svg?react';
+import CautionIcon from '@/assets/icons/Caution.svg?react';
+import DesktopIcon from '@/assets/icons/Desktop.svg?react';
+import UserIcon from '@/assets/icons/user-outline.svg?react';
+
 import { SettingsContent } from './SettingsContent';
 import { SettingsSection } from './SettingsSection';
+import { SettingsSectionHeader } from './SettingsSectionHeader';
 
 export function SettingsView() {
   return (
     <SettingsContent>
-      <SettingsSection>화면 섹션</SettingsSection>
-      <SettingsSection>알림 섹션</SettingsSection>
-      <SettingsSection>계정 관리 섹션</SettingsSection>
-      <SettingsSection>회원 탈퇴 섹션</SettingsSection>
+      <SettingsSection>
+        <SettingsSectionHeader icon={DesktopIcon} title="화면" />
+      </SettingsSection>
+
+      <SettingsSection>
+        <SettingsSectionHeader icon={BellIcon} title="알림" />
+      </SettingsSection>
+
+      <SettingsSection>
+        <SettingsSectionHeader icon={UserIcon} title="계정 관리" />
+      </SettingsSection>
+
+      <SettingsSection>
+        <SettingsSectionHeader icon={CautionIcon} title="회원 탈퇴" />
+      </SettingsSection>
     </SettingsContent>
   );
 }
