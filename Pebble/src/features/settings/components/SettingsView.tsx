938503e7dd8d084743c4d1f9cea7aea31cf408f3
Chae102 <coth4498@naver.com>
2026-07-09 21:05:48 +0900

    refactor: 회원탈퇴 섹션을 컴포넌트로 교체

--- Pebble/src/features/settings/components/SettingsView.tsx
+++ Pebble/src/features/settings/components/SettingsView.tsx
@@ -1,14 +1,8 @@
-import CautionIcon from '@/assets/icons/Caution.svg?react';
-
-import { Button } from '@/components/ui/Button';
-
 import { AccountSettingsSection } from './AccountSettingsSection';
 import { DisplaySettingsSection } from './DisplaySettingsSection';
 import { NotificationSettingsSection } from './NotificationSettingsSection';
 import { SettingsContent } from './SettingsContent';
-import { SettingsSection } from './SettingsSection';
-import { SettingsSectionHeader } from './SettingsSectionHeader';
-import { SettingsRow } from './SettingsRow';
+import { WithdrawalSection } from './WithdrawalSection';
 
 const MOCK_EMAIL = 'example1234@naver.com';
 const MOCK_IS_SOCIAL_ACCOUNT = false;
@@ -25,17 +19,7 @@ export function SettingsView() {
         isSocialAccount={MOCK_IS_SOCIAL_ACCOUNT}
       />
 
-      <SettingsSection className="min-h-[179px]">
-        <SettingsSectionHeader icon={CautionIcon} title="회원 탈퇴" />
-
-        <div className="mt-token-l flex flex-col gap-token-l">
-          <SettingsRow
-            title="회원 탈퇴"
-            description="모든 데이터가 삭제되며, 복구할 수 없어요"
-            actions={<Button variant="danger">탈퇴하기</Button>}
-          />
-        </div>
-      </SettingsSection>
+      <WithdrawalSection />
     </SettingsContent>
   );
 }
