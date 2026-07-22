77bd9a1be0dac4899f49abb663dd2da36cdb8ec0
Chae102 <coth4498@naver.com>
2026-07-22 09:43:07 +0900

    feat: 랜딩 페이지 기본 구조 추가

--- Pebble/src/App.tsx
+++ Pebble/src/App.tsx
@@ -9,6 +9,7 @@ import { SignUpCompletePage } from '@/features/auth/pages/SignUpCompletePage';
 import SettingsPage from './pages/settings/SettingsPage';
 import MyPage from "@/pages/mypage/MyPage";
 import ProfileEditPage from "@/pages/mypage/ProfileEditPage";
+import { LandingPage } from '@/pages/landing/LandingPage';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
       {/* 테마 스위칭을 테스트하려면 아래 div에 className="theme-popart" 등을 추가하세요 */}
       <div className="min-h-screen font-sans">
         <Routes>
+          <Route path="/landing" element={<LandingPage />} />
+
           <Route element={<MainLayout />}>
             <Route index element={<CalendarMainPage />} />
             <Route path="my" element={<MyPage />} />
