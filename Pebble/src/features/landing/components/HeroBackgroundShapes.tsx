08db9f0eb7b08f17ceff1e0e43a8f80799291ab8
Chae102 <coth4498@naver.com>
2026-07-22 10:04:35 +0900

    feat: 랜딩 Hero 배경 shape 구현

---
+++ Pebble/src/features/landing/components/HeroBackgroundShapes.tsx
@@ -0,0 +1,10 @@
+export function HeroBackgroundShapes() {
+  return (
+    <div aria-hidden="true" className="pointer-events-none absolute inset-0">
+      <div className="absolute left-[400px] top-[-209px] h-[499px] w-[585px] rotate-[70deg] rounded-[135px] bg-fill-surface opacity-70" />
+      <div className="absolute left-[847px] top-[37px] h-[680px] w-[325px] -rotate-[67deg] rounded-[183px] bg-fill-surface" />
+      <div className="absolute left-[1136px] top-[502px] h-[273px] w-[413px] -rotate-[11deg] rounded-[183px] bg-fill-surface opacity-90" />
+      <div className="absolute left-[-304px] top-[997px] h-[680px] w-[325px] rotate-[72deg] rounded-[183px] bg-fill-surface" />
+    </div>
+  );
+}
