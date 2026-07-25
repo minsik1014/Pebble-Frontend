31dfc9920b633b7a3f860f97ed9c4a888e38dbd7
Chae102 <coth4498@naver.com>
2026-07-25 14:53:08 +0900

    design: FinalCTASection svg 디자인 수정

--- Pebble/src/features/landing/components/FinalCTASection.tsx
+++ Pebble/src/features/landing/components/FinalCTASection.tsx
@@ -17,14 +17,14 @@ function FinalCTABackgroundShapes() {
       <img
         src={ellipse8}
         alt=""
-        className="absolute left-[-398px] top-[-107px] h-[1281px] w-[731px] object-contain"
+        className="absolute left-[-398px] top-[-107px] h-[1281px] w-[731px] rotate-[-90deg] object-contain opacity-[0.05] blur-[50px]"
       />
 
       {/* Ellipse 7 */}
       <img
         src={ellipse7}
         alt=""
-        className="absolute left-[606px] top-[471px] h-[1260px] w-[693px] object-contain"
+        className="absolute left-[606px] top-[471px] h-[1260px] w-[693px] rotate-[-90deg] object-contain opacity-10 blur-[50px]"
       />
     </div>
   );
