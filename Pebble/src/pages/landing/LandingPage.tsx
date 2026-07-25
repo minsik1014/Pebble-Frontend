d361e35f272e76209c87dd48905c5fa4cc67e9f8
Chae102 <coth4498@naver.com>
2026-07-25 14:51:10 +0900

    feat: 랜딩 페이지에 최종 CTA 섹션 연결

--- Pebble/src/pages/landing/LandingPage.tsx
+++ Pebble/src/pages/landing/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { PublicHeader } from '@/components/layout/PublicHeader';
+import { FinalCTASection } from '@/features/landing/components/FinalCTASection';
 import { HeroSection } from '@/features/landing/components/HeroSection';
 import { LandingFigmaSection } from '@/features/landing/components/LandingFigmaSection';
 import { LandingShell } from '@/features/landing/components/LandingShell';
@@ -18,6 +19,10 @@ export function LandingPage() {
 
         <LandingFigmaSection height={1024}>
           <StepStructureSection />
+        </LandingFigmaSection>
+
+        <LandingFigmaSection height={1024}>
+          <FinalCTASection />
         </LandingFigmaSection>
       </div>
     </LandingShell>
