cfc549a9af0febff5f20434325d11d87e8978957
Chae102 <coth4498@naver.com>
2026-07-26 16:03:37 +0900

    feat: 랜딩 히어로 텍스트 등장 애니메이션 적용

--- Pebble/src/features/landing/components/HeroBackgroundShapes.tsx
+++ Pebble/src/features/landing/components/HeroBackgroundShapes.tsx
@@ -1,9 +1,18 @@
 // src/features/landing/components/HeroBackgroundShapes.tsx
 
+import type { CSSProperties } from 'react';
+
 import vector1 from '@/assets/images/landing/hero-pebble-vector-1.svg';
 import vector2 from '@/assets/images/landing/hero-pebble-vector-2.svg';
 import vector3 from '@/assets/images/landing/hero-pebble-vector-3.svg';
 import vector4 from '@/assets/images/landing/hero-pebble-vector-4.svg';
+
+function popInStyle(targetOpacity: string, delay: number): CSSProperties {
+  return {
+    '--landing-hero-target-opacity': targetOpacity,
+    animationDelay: `${delay}ms`,
+  } as CSSProperties;
+}
 
 export function HeroBackgroundShapes() {
   return (
@@ -15,28 +24,32 @@ export function HeroBackgroundShapes() {
       <img
         src={vector3}
         alt=""
-        className="absolute left-[400px] top-[-80px] h-[498.912px] w-[585.439px] object-contain"
+        style={popInStyle('1', 900)}
+        className="landing-hero-pop-in absolute left-[400px] top-[-80px] h-[498.912px] w-[585.439px] object-contain"
       />
 
       {/* Vector 1 */}
       <img
         src={vector1}
         alt=""
-        className="absolute left-[927px] top-[-30px] h-[680.443px] w-[550.439px] object-contain"
+        style={popInStyle('1', 1050)}
+        className="landing-hero-pop-in absolute left-[927px] top-[-30px] h-[680.443px] w-[550.439px] object-contain"
       />
 
       {/* Vector 2 */}
       <img
         src={vector2}
         alt=""
-        className="absolute left-[1036px] top-[512px] h-[273.221px] w-[512.699px] object-contain"
+        style={popInStyle('1', 1200)}
+        className="landing-hero-pop-in absolute left-[1036px] top-[512px] h-[273.221px] w-[512.699px] object-contain"
       />
 
       {/* Vector 4 */}
       <img
         src={vector4}
         alt=""
-        className="absolute left-[0px] top-[906.62px] h-[700.443px] w-[325.439px] object-contain"
+        style={popInStyle('1', 1650)}
+        className="landing-hero-pop-in absolute left-[0px] top-[906.62px] h-[700.443px] w-[325.439px] object-contain"
       />
     </div>
   );
