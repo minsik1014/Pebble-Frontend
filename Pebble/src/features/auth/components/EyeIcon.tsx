9afb1d92116690e033b5f660c9ccb80347d04ba6
yoona24 <estelle0204@naver.com>
2026-08-01 21:57:21 +0900

    fix: 눈 아이콘 import 경로 수정

--- Pebble/src/features/auth/components/EyeIcon.tsx
+++ Pebble/src/features/auth/components/EyeIcon.tsx
@@ -1,4 +1,4 @@
-import eyesIcon from '@/assets/icons/Ic/Ic/Eyes.svg';
+import eyesIcon from '@/assets/icons/Ic/Eyes.svg';
 
 /** 로그인 비밀번호 표시 토글에 사용하는 디자인 시스템 눈 아이콘입니다. */
 export const EyeIcon = ({ open: _open }: { open: boolean }) => (
