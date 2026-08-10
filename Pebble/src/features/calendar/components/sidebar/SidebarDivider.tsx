b433f2806df1e6639751bedf9a2b94abd20fdf4a
Chae102 <coth4498@naver.com>
2026-08-11 01:11:06 +0900

    fix: GNB 구분선 색상 변경

--- Pebble/src/features/calendar/components/sidebar/SidebarDivider.tsx
+++ Pebble/src/features/calendar/components/sidebar/SidebarDivider.tsx
@@ -10,7 +10,7 @@ export const SidebarDivider = ({ visible }: SidebarDividerProps): JSX.Element | 
   return (
     <div
       aria-hidden="true"
-      className="absolute bottom-3 left-[84px] top-[100px] z-20 w-px bg-btn-quaternary"
+      className="absolute bottom-3 left-[84px] top-[100px] z-20 w-px bg-btn-quaternary dark:bottom-0 dark:top-0 dark:bg-border-teritory"
     />
   );
 };
